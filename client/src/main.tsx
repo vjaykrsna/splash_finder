c98c96b404f245a047bb4eaf892aad23331a9b92
vjaykrsna <vijay74684@gmail.com>
2025-11-04 22:11:14 +0530

    feat(client): add global CSS styles for application UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRouter } from './router/AppRouter.js';
 import { AuthProvider } from './providers/AuthProvider.js';
+import './styles/global.css';
 
 const container = document.getElementById('root');
 
